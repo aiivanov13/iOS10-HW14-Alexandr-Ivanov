e73d4dbfed449bf8feff3b7d41e7dcb519f73d4f
aivanov <alex130896@gmail.com>
2023-07-03 22:07:49 +0400

    Create project structure
    Добавил структуру проекта

---
+++ iOS10-HW14-Alexandr-Ivanov/Sources/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  iOS10-HW14-Alexandr-Ivanov
+//
+//  Created by Александр Иванов on 03.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHeirarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+
+    private func setupHeirarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+}
