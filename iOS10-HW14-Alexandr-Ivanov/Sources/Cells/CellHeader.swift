ddb1965e080a3f58182d99dde5b940d69c350bc2
aivanov <alex130896@gmail.com>
2023-07-05 23:40:57 +0400

    Create header

---
+++ iOS10-HW14-Alexandr-Ivanov/Sources/Cells/CellHeader.swift
@@ -0,0 +1,87 @@
+//
+//  CellHeader.swift
+//  iOS10-HW14-Alexandr-Ivanov
+//
+//  Created by Александр Иванов on 04.07.2023.
+//
+
+import UIKit
+
+class CellHeader: UICollectionReusableView {
+    var title: String? {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    var buttonIsHiden: Bool? {
+        didSet {
+            button.isHidden = buttonIsHiden ?? true
+        }
+    }
+
+    static let identifier = "CellHeader"
+
+    // MARK: - Outlets
+
+    private let separator: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray3
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("See All", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        button.isHidden = false
+        return button
+    }()
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        isUserInteractionEnabled = true
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Error in Cell")
+    }
+
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+        addSubview(separator)
+        addSubview(titleLabel)
+        addSubview(button)
+    }
+
+    private func setupLayout() {
+        separator.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.leading.equalTo(self).inset(10)
+            make.trailing.top.equalTo(self)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self).inset(10)
+            make.centerY.equalTo(self)
+        }
+
+        button.snp.makeConstraints { make in
+            make.trailing.equalTo(self).inset(20)
+            make.centerY.equalTo(self)
+        }
+    }
+}
